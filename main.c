fca815913589a40694cfe9b7b0b943d08091dc11
Ignacio <rodriguez.ignacio.dse@uteq.edu.mx>
2018-09-07 23:49:05 -0500

    modificacion de main

--- main.c
+++ main.c
@@ -1,3 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
 
-pruebas de modificacion
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+void multiplicar(){
+	float valorUno=0;
+	float valorDos=0;
+	float resultado=0;
+	
+	printf("Ingrese el valor uno:");
+	scanf("%f",&valorUno);
+	printf("Ingrese el valor dos:");
+	scanf("%f",&valorDos);
+	
+	resultado=valorUno*valorDos;
+	
+	printf("El resultado de multiplicar %f y %f es:%f\n",valorUno,valorDos,resultado);
+	
+}
+
+void main(int argc, char *argv[]) {
+	char opc;
+	
+	do{
+		multiplicar();
+		printf("Desea multiplicar nuevamente:");
+		scanf("%s",&opc);
+
+	}
+	while((opc=='s')||(opc=='S'));
+}
 
